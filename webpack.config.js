7ce5cd7a5d8117438b984b68783cbb95c9af0117
Justin Thornton <jathom15@gmail.com>
2022-01-02 18:16:24 -0600

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+mode: "development",
+entry: {
+  index: './src/index.js',
+},
+devtool: 'inline-source-map',
+plugins: [
+  new HtmlWebpackPlugin({
+    title: 'Restaurant-Page',
+  }),
+],
+output: {
+  filename: '[name].bundle.js',
+  path: path.resolve(__dirname, 'dist'),
+  clean: true,
+  },
+};
