5a1a2ec61cbdae1fc2947875eca2072e4942fdb5
Justin Thornton <jathom15@gmail.com>
2022-01-04 21:40:23 -0600

    removing comments

--- src/menu.js
+++ src/menu.js
@@ -7,22 +7,6 @@ function component() {
 	const Bratwurst = menuRender("Bratwurst");
 	const VeganDog = menuRender("Vegan Dog");
 
-	// <div class="diamond">
-	// 			<div class="menu-item">Eddie Dog</div>
-	// 		</div>
-	// 		<div class="diamond">
-	// 			<div class="menu-item">Chilli Dog</div>
-	// 		</div>
-	// 		<div class="diamond">
-	// 			<div class="menu-item">Corn Dog</div>
-	// 		</div>
-	// 		<div class="diamond">
-	// 			<div class="menu-item">Bratwurst</div>
-	// 		</div>
-	// 		<div class="diamond">
-	// 			<div class="menu-item">Vegan Dog</div>
-	// 		</div>
-
 	return { EddieDog, ChilliDog, CornDog, Bratwurst, VeganDog };
 }
 export { component };
