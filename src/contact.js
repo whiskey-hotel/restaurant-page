ee28f0581d9b90e2b9b6843e6cbf7ccec21269d4
Justin Thornton <jathom15@gmail.com>
2022-01-13 15:09:35 -0600

    Add render module

--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,9 @@
-import { newElement } from "./styleSet";
+import { nestedElement, newElement } from "./styleSet";
+
+function contactUs(){
+    const slogan = nestedElement("div", "div", "diamond", "aboutUs-item");
+}
 
 const heyContactUs = newElement("div", ...Array(2), "Contact Us");
 
-export { heyContactUs };
+export { heyContactUs, contactUs };
--- src/index.js
+++ src/index.js
@@ -2,51 +2,20 @@ import "./index.css";
 import { menu } from "./menu";
 import { aboutUs } from "./aboutUs";
 import { heyContactUs } from "./contact";
-import { newElement, nestedElement } from "./styleSet";
-
-const header = newElement("header");
-const title = newElement("h1", ...Array(2), "Eddies Million Dollar Dogs");
-const nav = newElement("nav", ...Array(1), "nav-bar");
-const menuA = nestedElement("div", "a", "nav-item-div", "nav-item", ...Array(1), "Menu");
-const aboutA = nestedElement("div", "a", "nav-item-div", "nav-item", ...Array(1), "About Us");
-const contactA = nestedElement("div", "a", "nav-item-div", "nav-item", ...Array(1), "Contact Us");
-const main = newElement("div", ...Array(1), "content");
-
-document.body.appendChild(header);
-header.appendChild(title);
-header.appendChild(nav);
-nav.appendChild(menuA);
-nav.appendChild(aboutA);
-nav.appendChild(contactA);
-document.body.appendChild(main);
-
-function removeAllChildNodes(parent) {
-	while (parent.firstChild) {
-		parent.removeChild(parent.firstChild);
-	}
-}
-
-function moduleRender(obj) {
-	removeAllChildNodes(main);
-	const moduleObject = obj;
-	for (let x in moduleObject) {
-		main.appendChild(moduleObject[x]);
-	}
-}
-
-//menu module
-menuA.addEventListener("click", function () {
-	moduleRender(menu());
-});
-//about us module
-aboutA.addEventListener("click", function () {
-	moduleRender(aboutUs());
-});
-//contact us module
-contactA.addEventListener("click", function () {
-	removeAllChildNodes(main);
-	main.appendChild(heyContactUs);
-});
+import { pageRender } from "./render";
 
 //initial render
-moduleRender(menu());
+pageRender.moduleRender(menu())
+
+//menu module
+pageRender.menuA.addEventListener("click", function () {
+	pageRender.moduleRender(menu());
+});
+//about us module
+pageRender.aboutA.addEventListener("click", function () {
+	pageRender.moduleRender(aboutUs());
+});
+//contact us module
+pageRender.contactA.addEventListener("click", function () {
+	pageRender.moduleRender(heyContactUs)
+});
